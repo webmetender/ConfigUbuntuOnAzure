69892eaeadb5c035a3578c2d2ddf14d75bc67c23
Adam Kuroczycki <webmetender@gmail.com>
2017-12-07 16:23:22 -0500

    Update 02_worker_compile_config.sh

--- 02_worker_compile_config.sh
+++ 02_worker_compile_config.sh
@@ -2,7 +2,10 @@
 git clone https://github.com/fireice-uk/xmr-stak.git
 echo "Enter below your desired donation percentage (2.0 by default) and press [ENTER]:"
 read donationPerc
-sudo echo -e "#pragma once\n\nconstexpr double fDevDonationLevel = $donationPerc / 100.0;\n" > xmr-stak/xmrstak/donate-level.hpp
-mkdir xmr-stak/build
+sudo echo -e "#pragma once\n\nconstexpr double fDevDonationLevel = $donationPerc / 100.0;\n" > ./xmr-stak/xmrstak/donate-level.hpp
+sudo sed -i 's/"use_slow_memory" : "warn"/"use_slow_memory" : "no_mlck"/g' ./xmr-stak/xmrstak/config.tpl
+sudo mkdir xmr-stak/build
 cd xmr-stak/build
-
+sudo cmake -DCMAKE-BUILD_TYPE=RELEASE -DMICROHTTPD_ENABLE=ON -DXMR-STAK_CURRENCY=monero -DXMR-STAK_COMPILE=native -DCPU_ENABLE=ON -DHWLOC_ENABLE=ON -DOpenCL_ENABLE=OFF -DCUDA_ENABLE=OFF ..
+sudo make install
+sudo ./bin/xmr-stak
