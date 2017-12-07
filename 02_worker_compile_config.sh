2bb2311e5bb469fb4b56a784c92aeddd843c00e3
Adam Kuroczycki <webmetender@gmail.com>
2017-12-07 13:23:44 -0500

    Update 02_worker_compile_config.sh

--- 02_worker_compile_config.sh
+++ 02_worker_compile_config.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-
-
+git clone https://github.com/fireice-uk/xmr-stak.git
+echo Enter below your desired donation percentage (2.0 by default) and press [ENTER]:
+read donationPerc
+sudo echo $'#pragma once\n\nconstexpr double fDevDonationLevel = ' > xmr-stak/xmrstak/donate-level.hpp
+sudo echo $donationPerc >> xmr-stak/xmrstak/donate-level.hpp
+sudo echo $' / 100.0;\n' >> xmr-stak/xmrstak/donate-level.hpp
+mkdir xmr-stak/build
+cd xmr-stak/build
 
 
 
