9f6eab833e7532c88a659956dfc64129f988bbfc
Adam Kuroczycki <webmetender@gmail.com>
2017-12-12 10:33:28 -0500

    Update 01_sys_install_config.sh

--- 01_sys_install_config.sh
+++ 01_sys_install_config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install locate mlocate
+sudo apt-get --yes --force-yes install locate mlocate
 sudo updatedb
-sudo apt-get install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev
+sudo apt-get --yes --force-yes install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev
 sudo sysctl -w vm.nr_hugepages=128
 sudo echo $'* soft memlock 262144\n* hard memlock 262144\n' >> /etc/security/limits.conf
